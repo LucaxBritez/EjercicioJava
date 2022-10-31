6ac4fdf8439778a12da2722c329c5ef60ca3d0c4
LucaxBritez <britezlucaxavier@gmail.com>
2022-10-31 11:52:10 -0300

    Commit 1

---
+++ src/com/Ejercicio/Main.java
@@ -0,0 +1,33 @@
+package com.Ejercicio;
+
+public class Main {
+    public static void main(String[] args) {
+        //Numericos
+        byte num1 = 1;
+        short num2 = 2;
+        long num3 = 3;
+        int num4 = 4;
+        double num5 = 5;
+        float num6 = 1;
+        //Texto
+        String name = "Luca";
+        char name2 = 'l';
+
+        //Logicos
+        boolean bol1 = false;
+        boolean bol2 = true;
+
+        System.out.println("Este es un dato byte: "               + num1);
+        System.out.println("Este es un dato short: "              + num2);
+        System.out.println("Este es un dato long: "               + num3);
+        System.out.println("Este es un dato int: "                + num4);
+        System.out.println("Este es un dato double: "              + num5);
+        System.out.println("Este es un dato float: "               + num6);
+        System.out.println("Este es un dato STRING: "             + name);
+        System.out.println("Este es un dato char: "               + name2);
+        System.out.println("Este es un dato booleano falso: "     + bol1);
+        System.out.println("Este es un dato booleano verdadero: "  + bol2);
+
+
+    }
+}
